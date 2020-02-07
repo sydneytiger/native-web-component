ef434360be7eff5c792128538f84e978e4ca9866
Tiger Chen <tiger.chen@ing.com.au>
2020-02-07 14:57:24 +1100

    implementing star rating

---
+++ pages/RwStarRating.js
@@ -0,0 +1,142 @@
+class RwStarRating extends HTMLElement {
+  constructor() {
+    super();
+
+    this._root = this.attachShadow({mode: 'open'});
+
+    this._$top = null;
+    this._$bottom = null;
+
+    this._disabled = false;
+    this._value = 0;
+  }
+
+  connectedCallback() {
+    this._root.innerHTML = `
+      <style>
+        :host {
+          display: inline-block;
+          overflow: hidden;
+          width: 4.1em;
+          height: 1em;
+          user-select: none;
+          vertical-align: middle;
+          box-sizing: border-box;
+        }
+
+        .container {
+          color: #c5c5c5;
+          margin: 0 auto;
+          font-size: 1em;
+          line-height: 1em;
+          padding: 0;
+          position: relative;
+          cursor: pointer;
+        }
+
+        .container .top {
+          color: #e7bd06;
+          padding: 0;
+          position: absolute;
+          z-index: 1;
+          display: block;
+          top: 0;
+          left: 0;
+          overflow: hidden;
+          width: 0;
+        }
+
+        .container:hover .top {
+          display:none;
+        }
+
+        .container .bottom {
+          padding: 0;
+          position: absolute;
+          display: block;
+          top: 0;
+          left: 0;
+          unicode-bidi: bidi-override;
+          direction: rtl;
+        }
+
+        .container .bottom > span:hover,
+        .container .bottom > span:hover ~ span {
+          color: #e7bd06;
+        }
+
+        :host([disabled]) .container {
+          cursor: inherit;
+        }
+
+        :host([disabled]) .container .top {
+          display: block;
+        }
+
+        :host([disabled]) .container .bottom > span:hover,
+        :host([disabled]) .container .bottom > span:hover ~ span {
+          color: inherit;
+        }
+
+      </style>
+
+      <div class="container">
+        <div class="top">
+          <span>&#x2605</span>
+          <span>&#x2605</span>
+          <span>&#x2605</span>
+          <span>&#x2605</span>
+          <span>&#x2605</span>
+        </div>
+        <div class="bottom">
+          <span data-value="5">&#x2605</span>
+          <span data-value="4">&#x2605</span>
+          <span data-value="3">&#x2605</span>
+          <span data-value="2">&#x2605</span>
+          <span data-value="1">&#x2605</span>
+        </div>
+      </div>
+    `;
+
+    this._disabled = (this.getAttribute('disabled') !== null);
+    this._$top = this._root.querySelector('.top');
+  }
+
+  disconnectedCallback() {
+
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  set value(value) {
+    if (value !== this._value) {
+      this._value = value;
+      this._render();
+    }
+  }
+
+  static get observedAttributes() {
+    return ['disabled'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (newValue !== oldValue) {
+      switch (name) {
+        case 'disabled':
+          this._disabled = (newValue !== null);
+          break;
+      }
+
+    }
+  }
+
+  _render() {
+    if (this._$top) {
+      this._$top.style.width = (this._value * 20) + '%';
+    }
+  }
+}
+
+window.customElements.define('rw-star-rating', RwStarRating);
