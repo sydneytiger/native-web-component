cfcaf08ddb37be265deac562f1baa6a6d14275aa
Tiger Chen <tigerchen@Tigers-MBP-2.modem>
2020-02-11 13:04:57 +1100

    implemented slide menu

--- pages/RwSlideMenu.js
+++ pages/RwSlideMenu.js
@@ -4,14 +4,42 @@ class RwSlideMenu extends HTMLElement {
 
     this._root = this.attachShadow({mode: 'open'});
 
-    this._$frame = null
+    this._$frame = null;
+    this._$close = null;
 
     this._open = false;
+  }
+
+  set open(value) {
+    this._open = !!value;
+    this._render();
+  }
+
+  get open() {
+    return this._open;
   }
 
   connectedCallback() {
     this._root.innerHTML = `
     <style>
+    :host([theme="red"]) .title {
+      background-color:#e23f24;
+      color: #fff;
+    }
+
+    :host([theme="blue"]) .title {
+      background-color:#0d152d;
+      color: #fff;
+    }
+
+    :host([theme="red"]) .content-slot::slotted(a:hover) {
+      color: #e23f24;
+    }
+
+    :host([theme="blue"]) .content-slot::slotted(a:hover) {
+      color: #0d152d;
+    }
+
     .frame {
       position: fixed;
       top: 0;
@@ -23,7 +51,7 @@ class RwSlideMenu extends HTMLElement {
       transition: background-color 300ms ease-in;
     }
 
-    .contianer {
+    .container {
       width: 80%;
       max-width: 400px;
       background: #fff;
@@ -66,28 +94,67 @@ class RwSlideMenu extends HTMLElement {
       background-color: rgba(0, 0, 0, 0.25);
     }
 
-    .frame.open .contianer {
+    .frame.open .container {
       transform: none;
+    }
+
+    .content-slot {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .content-slot::slotted(a) {
+      color: #666;
+      border-bottom: solid 1px #f1f1f1
+      font-size: 16px;
+      font-weight: bold;
+      padding: 10px;
+      text-decoration: none;
+    }
+
+    .content-slot::slotted(a:hover) {
+      color: #000;
+    }
+
+    .content-slot::slotted(.item) {
+      
     }
     </style>
     <div class="frame">
       <nav class="container">
         <div class="title">
-          <div class="title-content">Menu</div>
+          <div class="title-content">
+            <slot name="title">Menu</slot>
+          </div>
           <a class="close">&#10006;</a>
         </div>
         <div class="content">
-          <a href="#">Menu item one</a>
-          <a href="#">Menu item two</a>
-          <a href="#">Menu item three</a>
+          <slot class="content-slot"></slot>
         </div>
       </nav>
     </div>
     `;
+
+    this._$frame = this._root.querySelector('.frame');
+    this._$close = this._root.querySelector('.close');
+    this._$close.addEventListener('click', e => {
+      if(this.open) {
+        this.open = false;
+      }
+    })
   }
 
   _render() {
-
+    if(this._$frame){
+      
+      if(this._open) {
+        this._$frame.classList.add('open');
+        this.dispatchEvent(new CustomEvent('menu-opened'));
+      } else {
+        this._$frame.classList.remove('open');
+        this.dispatchEvent(new CustomEvent('menu-closed'));
+      }
+    }
   }
 }
 
